9758f0a8a6cc26f79fb50b2879c15a42cf25f21c
Simon <kh931623@hotmail.com>
2024-01-05 15:27:54 +0800

    feat(homework1): finish credit-card module

--- homework1/src/CreditCard.hs
+++ homework1/src/CreditCard.hs
@@ -1,6 +1,10 @@
 module CreditCard
   ( toDigitsRev,
     toDigits,
+    doubleEveryOther,
+    testFunc,
+    sumDigits,
+    validate,
   )
 where
 
@@ -13,3 +17,42 @@ toDigits :: Integer -> [Integer]
 toDigits n
   | n <= 0 = []
   | otherwise = reverse (toDigitsRev n)
+
+doubleEveryOtherEvenVersion :: [Integer] -> [Integer]
+doubleEveryOtherEvenVersion [] = []
+doubleEveryOtherEvenVersion [x] = [x]
+doubleEveryOtherEvenVersion [x, y] = [x * 2, y]
+doubleEveryOtherEvenVersion (x : y : xs) = x * 2 : y : doubleEveryOtherEvenVersion xs
+
+doubleEveryOtherSingleVersion :: [Integer] -> [Integer]
+doubleEveryOtherSingleVersion [] = []
+doubleEveryOtherSingleVersion [x] = [x]
+doubleEveryOtherSingleVersion [x, y] = [x * 2, y]
+doubleEveryOtherSingleVersion (x : y : xs) = x : y * 2 : doubleEveryOtherEvenVersion xs
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther [x, y] = [x * 2, y]
+doubleEveryOther xs
+  | isLengthEven = doubleEveryOtherEvenVersion xs
+  | otherwise = doubleEveryOtherSingleVersion xs
+  where
+    isLengthEven = even (length xs)
+
+sumDigitsForInt :: Integer -> Integer
+sumDigitsForInt n
+  | n <= 0 = 0
+  | otherwise = sum (toDigitsRev n)
+
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . map sumDigitsForInt
+
+validate :: Integer -> Bool
+validate n = mod total 10 == 0
+  where
+    total = (sumDigits . doubleEveryOther . toDigits) n
+
+testFunc :: Int -> String
+testFunc 1 = "Hello"
+testFunc _ = "World!"
--- homework1/test/CreditCardSpec.hs
+++ homework1/test/CreditCardSpec.hs
@@ -1,13 +1,21 @@
 module CreditCardSpec (spec) where
 
 import CreditCard
-  ( toDigits,
+  ( doubleEveryOther,
+    sumDigits,
+    testFunc,
+    toDigits,
     toDigitsRev,
+    validate,
   )
 import Test.Hspec
 
 spec :: Spec
 spec = do
+  describe "testFucn" $ do
+    it "should return 'Hello' when given 1" $ do
+      testFunc 1 `shouldBe` "Hello"
+
   describe "toDigitsRev" $ do
     it "should return [] when given 0" $ do
       toDigitsRev 0 `shouldBe` []
@@ -21,3 +29,30 @@ spec = do
   describe "toDigits" $ do
     it "should return [] when given 0" $ do
       toDigits 0 `shouldBe` []
+
+  describe "doubleEveryOther" $ do
+    it "should return [] when given []" $ do
+      doubleEveryOther [] `shouldBe` []
+
+    it "should return the input when given only one element" $ do
+      doubleEveryOther [1] `shouldBe` [1]
+
+    it "should return [2, 3] when given [1, 3]" $ do
+      doubleEveryOther [1, 3] `shouldBe` [2, 3]
+
+    it "should return [16, 7, 12, 5] when given [8, 7, 6, 5]" $ do
+      doubleEveryOther [8, 7, 6, 5] `shouldBe` [16, 7, 12, 5]
+
+    it "should return [1, 4, 3] when given [1, 2, 3]" $ do
+      doubleEveryOther [1, 2, 3] `shouldBe` [1, 4, 3]
+
+  describe "sumDigits" $ do
+    it "should return 22 when given [16, 7, 12, 5]" $ do
+      sumDigits [16, 7, 12, 5] `shouldBe` 22
+
+  describe "validate" $ do
+    it "should return true when given 4012888888881881" $ do
+      validate 4012888888881881 `shouldBe` True
+
+    it "should return true when given 4012888888881882" $ do
+      validate 4012888888881882 `shouldBe` False
