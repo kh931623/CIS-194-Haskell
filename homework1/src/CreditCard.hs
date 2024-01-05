4ac79f5f4e69e3d8e64d6c97a524d5d0d1a3f55b
Simon <kh931623@hotmail.com>
2024-01-05 10:40:42 +0800

    feat: implement toDigitsRev

---
+++ homework1/src/CreditCard.hs
@@ -0,0 +1,9 @@
+module CreditCard
+  ( toDigitsRev,
+  )
+where
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n
+  | n <= 0 = []
+  | otherwise = mod n 10 : toDigitsRev (div n 10)
--- homework1/test/Spec.hs
+++ homework1/test/Spec.hs
@@ -1,2 +1,14 @@
+import CreditCard (toDigitsRev)
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "toDigitsRev" $ do
+    it "should return empty list when given 0" $ do
+      toDigitsRev 0 `shouldBe` []
+
+    it "should return [4, 3, 2, 1] when given 1234" $ do
+      toDigitsRev 1234 `shouldBe` [4, 3, 2, 1]
+
+    it "should return [] when given negative numbers" $ do
+      toDigitsRev (-17) `shouldBe` []
