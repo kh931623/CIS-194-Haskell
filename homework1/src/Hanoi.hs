6f826e2bc4d2d0be8bb5bcdd2cddb0e0fb0ed072
Simon Chen <simon.chen@pickupp.io>
2024-01-14 01:13:48 +0800

    misc(homework1): enable formatting

--- homework1/src/Hanoi.hs
+++ homework1/src/Hanoi.hs
@@ -1,10 +1,12 @@
-module Hanoi (
-  hanoi,
-)
+module Hanoi
+  ( hanoi,
+  )
 where
 
 type Peg = String
+
 type Move = (Peg, Peg)
+
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi 1 a b _ = [(a, b)]
