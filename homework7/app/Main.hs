5b2554cadd235dcaea736c2bdecfd14d3455f017
Simon <kh931623@hotmail.com>
2024-03-25 17:40:37 +0800

    feat(hw7): import lecture assets

--- homework7/app/Main.hs
+++ homework7/app/Main.hs
@@ -1,6 +1,13 @@
-module Main (main) where
+module Main where
 
-import Lib
+import Editor
+import StringBuffer
 
-main :: IO ()
-main = someFunc
+main =
+  runEditor editor $
+    unlines
+      [ "This buffer is for notes you don't want to save, and for",
+        "evaluation of steam valve coefficients.",
+        "To load a different file, type the character L followed",
+        "by the name of the file."
+      ]
